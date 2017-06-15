88a428352f42fbf5318a720ed5c5bdbf93db8036
Bruno Rodrigo <bnorodrigo@gmail.com>
2017-06-15 12:30:01 -0300

    Inclusão do getter setter nome

--- src/Cliente.java
+++ src/Cliente.java
@@ -5,6 +5,14 @@ public class Cliente {
 	private String cpf;
 	private int idade;
 	
+	public String getNome(){
+		return this.nome;
+	}
+	
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+		
 	public void mudaCPF(String cpf){
 		if(this.idade <= 60){
 			validaCPF(cpf);
