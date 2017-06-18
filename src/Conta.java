16cc02a23e5522f16ec3f46d270ccae739d9a737
Bruno Rodrigo <bnorodrigo@gmail.com>
2017-06-18 11:25:35 -0300

    Inclusão do getTotalDeContas

--- src/Conta.java
+++ src/Conta.java
@@ -3,23 +3,43 @@ public class Conta {
 	private double saldo;
 	private double limite;
 	private Cliente titular;
+	private static int totalDeContas;
 	
+	public Conta(){
+		Conta.totalDeContas ++;
+	}
 	public Conta(Cliente titular) {
 		this.titular = titular;
 	}
 	
+	public Conta(int numero, Cliente titular){
+		this(titular);
+		this.numero = numero;
+	}
+	
+	public static int getTotalDeContas(){
+		return Conta.totalDeContas;
+	}
+	
+	public void setTitula(Cliente titular){
+		this.titular = titular;
+	}
 	public Cliente getTitular(){
 		return titular;
 	}
 	
+
+	
 	public double getSaldo(){
 		return this.saldo + this.limite;
 	}
-	public void saca(double quantidade){
+	public boolean saca(double quantidade){
 		if (quantidade > this.saldo + this.limite){
 			System.out.println("Não posso sacar fora do limite");
+			return false;
 		}else {
-			this.saldo -= quantidade; 
+			this.saldo -= quantidade;
+			return true;
 		}
 	}
 	
