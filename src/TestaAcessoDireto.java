1bce634038c47f8201e02f128f463984ea3f110a
Bruno Rodrigo <bnorodrigo@gmail.com>
2017-06-18 11:28:32 -0300

    alterando a chamada para setSaldo

--- src/TestaAcessoDireto.java
+++ src/TestaAcessoDireto.java
@@ -1,17 +1,8 @@
 
 public class TestaAcessoDireto {
-<<<<<<< HEAD
 	public static void main(String[] args) {
 		Conta minhaConta = new Conta();
-		minhaConta.saldo = 100;
-	}
-=======
-
-	public static void main(String[] args) {
-		Conta minhaConta = new Conta();
-		minhaConta.saldo = 1000;
+		minhaConta.setSaldo(1000);
 
 	}
-
->>>>>>> eb0beca56d99fc2797d2fe95f30a788ff97e92f4
 }
