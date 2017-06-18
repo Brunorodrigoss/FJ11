15581770b84b1d97c260c26f061db86fa55f3604
Bruno Rodrigo <bnorodrigo@gmail.com>
2017-06-18 11:24:57 -0300

    Alterando parâmetros para private e inclusão de getters e setters

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,20 +1,76 @@
 
 public class Funcionario {
-	String nome;
-	String departamento;
-	double salario;
-	Data dataEntrada = new Data();
-	String rg;
+	private String nome;
+	private String departamento;
+	private double salario;
+	private Data dataEntrada = new Data();
+	private String rg;
+	private int indentificador;
+	private static int contador;
 	
-	void recebeAumento(double aumento){
+	public Funcionario() {
+		contador += 1;
+		this.indentificador = contador;
+	}
+	
+	public Funcionario(String nome){
+		this.nome = nome;
+		contador += 1;
+		this.indentificador = contador;
+	}
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+	
+	public int getIdentificador(){
+		return this.indentificador;
+	}
+	
+	public String getNome(){
+		return this.nome;
+	}
+	
+	public void setDepartamento(String departamento){
+		this.departamento = departamento;
+	}
+	
+	public String getDepartamento(){
+		return this.departamento;
+	}
+	
+	public void setSalario(double salario){
+		this.salario = salario;
+	}
+	
+	public double getSalario(){
+		return this.salario;
+	}
+	
+	public void setDataEntrada(Data data){
+		this.dataEntrada = data;
+	}
+	
+	public Data getDataEntrada(){
+		return this.dataEntrada;
+	}
+	
+	public void setRg(String rg){
+		this.rg = rg;
+	}
+	
+	public String GetRg(){
+		return this.rg;
+	}
+	
+	public void recebeAumento(double aumento){
 		this.salario += aumento;
 	}
 	
-	double calculaGanhoAnual(){
+	private double calculaGanhoAnual(){
 		return this.salario * 12;
 	}
 	
-	void mostra(){
+	public void mostra(){
 		System.out.println("Nome: "+this.nome);
 		System.out.println("Salário: "+this.salario);
 		System.out.println("Ganho anual: "+calculaGanhoAnual());
